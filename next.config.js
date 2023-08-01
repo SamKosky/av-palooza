6a0c93c76b146f03410e6464f1daf4fef58eb873
Sam Kosky <sam.kosky@aginic.com>
2023-08-01 14:12:12 +1000

    update dependencies

--- next.config.js
+++ next.config.js
@@ -17,12 +17,7 @@
 module.exports = {
   images: {
     domains: [
-      'www.datocms-assets.com',
-      'a.storyblok.com',
-      'images.ctfassets.net',
-      'images.prismic.io',
-      'cdn.aglty.io',
-      'localhost' // For Strapi
+      'www.datocms-assets.com'
     ],
     imageSizes: [24, 64, 300]
   }
